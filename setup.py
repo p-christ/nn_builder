70852b6dcc574b468be2d60df0d5303ec8172bd1
p-christ <p.christodoulou2@gmail.com>
2019-04-03 14:11:57 +0100

    requirements and build files

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 import setuptools
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
 setuptools.setup(
-     name='executable_file',
-     version='0.1',
-     scripts=['executable_file'] ,
-     author="Petros Christodoulou",
-     author_email="p.christodoulou2@gmail.com",
-     description="Build neural networks in 1 line",
-     long_description=long_description,
-   long_description_content_type="text/markdown",
-     url="https://github.com/p-christ/nn_builder",
-     packages=setuptools.find_packages(),
-     classifiers=[
-         "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: MIT License",
-         "Operating System :: OS Independent",
-     ],
- )
+    name="nn_builder",
+    version="0.0.1",
+    author="Petros Christodoulou",
+    author_email="p.christodoulou2@gmail.com",
+    description="Build neural networks in 1 line",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/p-christ/nn_builder",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

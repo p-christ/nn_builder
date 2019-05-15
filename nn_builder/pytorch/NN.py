3d202af1bae7b4ae95beb446ef13f73b0565dea0
Josiah Laivins <jokellum@northstate.net>
2019-05-15 15:52:42 -0400

    Fixed compiler error...

--- nn_builder/pytorch/NN.py
+++ nn_builder/pytorch/NN.py
@@ -79,7 +79,7 @@ class NN(nn.Module, PyTorch_Base_Network):
 
     def forward(self, x):
         """Forward pass for the network"""
-        if not self.checked_forward_input_data_once: self.check_input_data_into_forward_once(x)
+        if not self.checked_forward_input_data_once: self.check_input_data_into_forward_once(x.cpu())
         if self.embedding_to_occur: x = self.incorporate_embeddings(x)
         for layer_ix, linear_layer in enumerate(self.hidden_layers):
             x = self.get_activation(self.hidden_activations, layer_ix)(linear_layer(x))
@@ -108,7 +108,7 @@ class NN(nn.Module, PyTorch_Base_Network):
             assert torch.sum(abs(data.float() - data_long.float())) < 0.0001, """Data columns to be embedded should be integer 
                                                                                 values 0 and above to represent the different 
                                                                                 classes"""
-        if self.input_dim > len(self.columns_of_data_to_be_embedded): assert isinstance(x, torch.FloatTensor), f'Incorrect Tensor type is {x}'
+        if self.input_dim > len(self.columns_of_data_to_be_embedded): assert isinstance(x, torch.FloatTensor), f'Incorrect Tensor type x is {type(x)} is {x}'
         assert len(x.shape) == 2, "X should be a 2-dimensional tensor"
         self.checked_forward_input_data_once = True #So that it doesn't check again
 
